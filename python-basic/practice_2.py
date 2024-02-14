43b711efa9df53cb7df27058d37e29274fb8ec5f
Neisser <villa.neisser@gmail.com>
2024-02-13 20:56:53 -0500

    ✨ feat: add practice 2

---
+++ python-basic/practice_2.py
@@ -0,0 +1,31 @@
+# Creating a list with mixed data types
+list = ["apple", "banana", "cherry", 1, 2, 3]
+print(list)
+
+# Creating a dictionary with key-value pairs
+dictionary = {
+  "name": "John",
+  "age": 25
+}
+print(dictionary)
+
+# Checking if a value exists in a list
+numeric_list = [1, 2, 3, 4, 5]
+if 2 in numeric_list:
+  print("Yes, 2 is in the list")
+
+# Concatenating lists
+new_numeric_list = numeric_list + [6, 7, 8, 9, 10]
+print(new_numeric_list)
+
+# Getting the type of a variable
+print("type of new_numeric_list: ", type(new_numeric_list))
+
+# Getting the length of a list
+print("length of new_numeric_list: ", len(new_numeric_list))
+
+#exercise 2: print the sum of two numbers
+a = 10
+b = 20
+print("sum of a and b: ", a + b)
+
